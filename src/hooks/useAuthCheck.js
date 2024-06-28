cb05ccd608ce5312c494ce6363d8382dc870acb4
Goutam Dogri <dogrig507@gmail.com>
2024-06-28 15:17:33 +0530

    useAuthcheck downd for temporarily

--- src/hooks/useAuthCheck.js
+++ src/hooks/useAuthCheck.js
@@ -1,20 +1,22 @@
+// This is for securing the route so that if user is not logged in at first they should login. but as it not working in production, it is commented for temporarily
+
 import { useNavigate } from "react-router-dom";
 
 async function useAuthCheck() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	try {
-		const res = await fetch(
-			"http://localhost:8000/api/v1/healthcheck/authCheck",
-			{ method: "GET", credentials: "include" }
-		);
-		if (!res.ok) {
-			navigate("/", { replace: true });
-		}
-	} catch (error) {
-		// navigate("/", { replace: true });
-      console.log(error);
-	}
+	// try {
+	// 	const res = await fetch(
+	// 		"http://localhost:8000/api/v1/healthcheck/authCheck",
+	// 		{ method: "GET", credentials: "include" }
+	// 	);
+	// 	if (!res.ok) {
+	// 		navigate("/", { replace: true });
+	// 	}
+	// } catch (error) {
+	// 	// navigate("/", { replace: true });
+  //     console.log(error);
+	// }
 }
 
 export default useAuthCheck;
