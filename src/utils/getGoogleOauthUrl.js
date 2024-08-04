8e5e08bd855f2639de04fe6af42ae9aa68a80d93
Goutam Dogri <dogrig507@gmail.com>
2024-08-04 22:10:30 +0530

    vercel env var compilance testing

--- src/utils/getGoogleOauthUrl.js
+++ src/utils/getGoogleOauthUrl.js
@@ -2,8 +2,8 @@ function getGoogleOauthUrl() {
 	const rootUrl = "https://accounts.google.com/o/oauth2/auth"
 
 	const options = {
-		redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
-		client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+		redirect_uri: import.meta.env.VITE_VERCEL_ENV_GOOGLE_REDIRECT_URI,
+		client_id: import.meta.env.VITE_VERCEL_ENV_GOOGLE_CLIENT_ID,
 		access_type: "offline",
     response_type: "code",
     prompt: "consent",
